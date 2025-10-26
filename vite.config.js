05ae126387f798b09d262e9cd2e29ce2e12637aa
xSp1der42 <nikitakambrionov@gmail.com>
2025-10-27 03:27:36 +0500

    гг

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist', // <- ВАЖНО! Точка выхода для Vercel
+    rollupOptions: {
+      input: '/index.html', // Чтобы Vite правильно нашёл корневой HTML
+    },
+  },
 })
